a54470888e0391890359a4f7f0b66916f6030830
robert-min <robertmin522@gmail.com>
2022-12-22 22:03:41 +0900

    goroutines study

---
+++ mdict/mdict.go
@@ -0,0 +1,48 @@
+package mdict
+
+import "errors"
+
+type Dictionary map[string]string
+
+var (
+	errNotFound   = errors.New("Not Found")
+	errCantUpdate = errors.New("Cant update non-exisiting word")
+	errWordExists = errors.New("That word already exist")
+)
+
+// Search for a word
+func (d Dictionary) Search(word string) (string, error) {
+	value, exists := d[word]
+	if exists {
+		return value, nil
+	}
+	return "", errNotFound
+}
+
+// Add a word to the dictionary
+func (d Dictionary) Add(word, def string) error {
+	_, err := d.Search(word)
+	if err == errNotFound {
+		d[word] = def
+	} else if err == nil {
+		return errWordExists
+	}
+	return nil
+}
+
+// Update a word
+func (d Dictionary) Update(word, definition string) error {
+	_, err := d.Search(word)
+	switch err {
+	case nil:
+		d[word] = definition
+	case errNotFound:
+		return errCantUpdate
+	}
+	return nil
+}
+
+// Delete a word
+func (d Dictionary) Delete(word string) {
+	delete(d, word)
+}
