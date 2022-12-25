7cfed579a5b0d72c0632cab37a2988917cd24636
robert-min <robertmin522@gmail.com>
2022-12-25 13:45:34 +0900

    learning crawling

--- main.go
+++ main.go
@@ -1,53 +1,42 @@
 package main
 
 import (
-	"errors"
 	"fmt"
+	"log"
 	"net/http"
+
+	"github.com/PuerkitoBio/goquery"
 )
 
-type result struct {
-	url    string
-	status string
-}
-
-var errRequestFailed = errors.New("Request Failed")
+var baseURL string = "https://kr.indeed.com/jobs?q=python&l=&vjk=1015284880e2ff62"
 
 func main() {
-	results := make(map[string]string)
-	c := make(chan result)
-	urls := []string{
-		"https://www.airbnb.com/",
-		"https://www.google.com/",
-		"https://www.amazon.com/",
-		"https://www.reddit.com/",
-		"https://www.google.com/",
-		"https://soundcloud.com/",
-		"https://www.facebook.com/",
-		"https://www.instagram.com/",
-		"https://academy.nomadcoders.co/",
-	}
-
-	for _, url := range urls {
-		go hitURL(url, c)
-	}
-
-	for i := 0; i < len(urls); i++ {
-		result := <-c
-		results[result.url] = result.status
-	}
-	for url, status := range results {
-		fmt.Println(url, status)
-	}
-
+	getPages()
 }
 
-func hitURL(url string, c chan result) {
-	fmt.Println("Checking", url)
-	resp, err := http.Get(url)
-	status := "OK"
-	if err == nil && resp.StatusCode >= 400 {
-		status = "FAILED"
+func getPages() int {
+	res, err := http.Get(baseURL)
+	checkErr(err)
+	checkCode(res)
+
+	defer res.Body.Close()
+
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	checkErr(err)
+
+	fmt.Println(doc)
+
+	return 0
+}
+
+func checkErr(err error) {
+	if err != nil {
+		log.Fatalln(err)
 	}
-	c <- result{url: url, status: status}
+}
+
+func checkCode(res *http.Response) {
+	if res.StatusCode != 200 {
+		log.Fatalln("Request failed with Status", res.StatusCode)
+	}
 }
